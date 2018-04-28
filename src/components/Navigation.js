e1e921a845601d6810a92bd59f448498d3d89b56
valeshan <valeshan@gmail.com>
2018-04-28 11:31:23 +1000

    fixed nav errors
    -removed nav import from react-bootstrap, as doesn't support directly
    nesting non-react-bootstrap dom elements.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
-import {Nav, Button} from 'react-bootstrap';
+import {Button} from 'react-bootstrap';
 
 
 
 
 const Navigation = props =>{
   return (
-    <div>
-      <Nav  className="main-nav">
+    <div className = 'main-nav'>
           <NavLink className = "link-style" to="/cats"><Button bsStyle = "primary" className = "categoryComponent">Cats</Button></NavLink>
           <NavLink className = "link-style" to="/dogs"><Button bsStyle = "primary" className = "categoryComponent">Dogs</Button></NavLink>
           <NavLink className = "link-style" to="/balloons"><Button bsStyle = "primary" className = "categoryComponent">Balloons</Button></NavLink>
           <NavLink className = "link-style" to="/search"><Button bsStyle = "primary" className = "categoryComponent">Search</Button></NavLink>
-      </Nav>
     </div>
   )
 }
